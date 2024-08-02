01b9a1b9684959d093c82189f22f4d47421f45b9
paimonli <paimonli@tencent.com>
2024-08-02 15:01:25 +0800

    完成初始化及触摸事件监听

---
+++ assets/script/block.ts
@@ -0,0 +1,66 @@
+import { _decorator, Color, Component, Label, Sprite } from 'cc';
+const { ccclass, property } = _decorator;
+
+
+@ccclass('Block')
+export class Block extends Component {
+    @property({ type: Label })
+    private numLabel: Label;
+
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+
+    }
+
+    setNumber(num: number) {
+        this.numLabel.node.active = true;
+        if (num == undefined) {
+            this.numLabel.node.active = false;
+            return;
+        } else if (num == 0) {
+            this.numLabel.node.active = false;
+        }
+        this.numLabel.string = num.toString();
+        this.node.getComponent(Sprite).color = colors.searchColor(num);
+    }
+
+    getNumber(): number {
+        return Number.parseInt(this.numLabel.string);
+    }
+}
+
+class ColorHolder {
+    private color = new Map<number, Color>();
+    private maxKey: number = 2048;
+    private defaultColor: Color = new Color(198, 184, 172, 255);
+
+    constructor() {
+        this.color.set(0, new Color(198, 184, 172, 255));
+        this.color.set(2, new Color(235, 224, 213, 255));
+        this.color.set(4, new Color(234, 219, 193, 255));
+        this.color.set(8, new Color(240, 167, 110, 255));
+        this.color.set(16, new Color(244, 138, 89, 255));
+        this.color.set(32, new Color(245, 112, 85, 255));
+        this.color.set(64, new Color(245, 83, 52, 255));
+        this.color.set(128, new Color(234, 200, 103, 255));
+        this.color.set(256, new Color(234, 197, 87, 255));
+        this.color.set(512, new Color(234, 192, 71, 255));
+        this.color.set(1024, new Color(146, 208, 80, 255));
+        this.color.set(2048, new Color(0, 176, 240, 255));
+    }
+
+    searchColor(num: number) {
+        while (num > this.maxKey) {
+            num - this.maxKey;
+        }
+        if (this.color.has(num)) {
+            return this.color.get(num);
+        }
+        return this.defaultColor;
+    }
+}
+
+var colors: ColorHolder = new ColorHolder();
--- assets/script/game.ts
+++ assets/script/game.ts
@@ -1,33 +1,28 @@
-import { _decorator, Color, Component, director, instantiate, Label, log, Node, Prefab, Size, UIRenderer, UITransform, view } from 'cc';
+import { _decorator, Color, Component, director, EventTouch, Input, instantiate, Label, log, Node, Prefab, Size, UIRenderer, UITransform, Vec2, view } from 'cc';
+import { Block } from './block';
 const { ccclass, property } = _decorator;
 
-@ccclass('game')
-export class game extends Component {
+@ccclass('Game')
+export class Game extends Component {
+    private level: number = 4;
+
     private _gapSize: number = 20;
     private _blockSize: number;
 
     @property({ type: Prefab })
     private block: Prefab = null;
 
-    private _scoreLabel: Label;
+    @property({ type: Label })
+    private scoreLabel: Label;
+
     private _score: number;
 
-    private color: Color[];
+    private blocks: Array<Array<Node>>;
 
     start() {
         this.draw();
-        this.color[0] = new Color(198, 184, 172, 255);
-        this.color[2] = new Color(235, 224, 213, 255);
-        this.color[4] = new Color(234, 219, 193, 255);
-        this.color[8] = new Color(240, 167, 110, 255);
-        this.color[16] = new Color(244, 138, 89, 255);
-        this.color[32] = new Color(245, 112, 85, 255);
-        this.color[64] = new Color(245, 83, 52, 255);
-        this.color[128] = new Color(234, 200, 103, 255);
-        this.color[256] = new Color(234, 197, 87, 255);
-        this.color[512] = new Color(234, 192, 71, 255);
-        this.color[1024] = new Color(146, 208, 80, 255);
-        this.color[2048] = new Color(0, 176, 240, 255);
+        this.init();
+        this.addTouchHandler();
     }
 
     update(deltaTime: number) {
@@ -36,18 +31,91 @@ export class game extends Component {
 
     draw() {
         let width: number = view.getVisibleSize().width;
+        let height: number = view.getVisibleSize().height;
+        let bottom = -height / 2
+        let left = -width / 2
         this._blockSize = (width - this._gapSize * 5) / 4;
-        let x = this._gapSize + this._blockSize / 2;
-        let y = this._blockSize;
-        // 初始化block
-        let block = instantiate(this.block);
-        let blockUI = block.getComponent(UITransform)
-        let blockSize = new Size(y, y);
-        blockUI.setContentSize(blockSize)
-        // 添加block
-        this.node.addChild(block);
-        block.setPosition(x, y);
+
+        this.blocks = new Array<Array<Node>>(this.level);
+        for (let row = 0; row < this.level; row++) {
+            if (this.blocks[row] == undefined) {
+                this.blocks[row] = new Array<Node>(4);
+            }
+            for (let col = 0; col < this.level; col++) {
+                this.blocks[row][col] = instantiate(this.block);
+                let blockUI = this.blocks[row][col].getComponent(UITransform)
+                let blockSize = new Size(this._blockSize, this._blockSize);
+                blockUI.setContentSize(blockSize)
+
+                let x = this._gapSize * (col + 1) + this._blockSize * col + left + this._blockSize / 2;
+                let y = this._gapSize * (row + 1) + this._blockSize * row + bottom + this._blockSize / 2;
+
+                this.blocks[row][col].setPosition(x, y);
+                this.node.addChild(this.blocks[row][col]);
+                let block = this.blocks[row][col].getComponent(Block);
+                block.setNumber(0);
+            }
+        }
+    }
+
+    /**
+     * 旋转二维数组
+     * @param blocks 二维数组
+     */
+    rotateBlocks(blocks: Array<Array<Node>>, direction) {
+
+    }
+
+    move() {
+
+    }
+
+    addTouchHandler() {
+        let startPoint: Vec2;
+        this.node.on(Input.EventType.TOUCH_START, (event: EventTouch) => {
+            startPoint = event.getLocation()
+        }, this)
+        this.node.on(Input.EventType.TOUCH_END, (event: EventTouch) => {
+            let endPoint = event.getLocation()
+            let moveDistance = Vec2.distance(endPoint, startPoint);
+
+            // TODO: 优化防抖
+            if (moveDistance > 50) {
+                if (Math.abs(startPoint.y - endPoint.y) < 50) {
+                    if (startPoint.x > endPoint.x) {
+                        log("向左")
+                    } else if (startPoint.x < endPoint.x) {
+                        log("向右")
+                    }
+                } else if (Math.abs(startPoint.x - endPoint.x) < 50) {
+                    if (startPoint.y > endPoint.y) {
+                        log("向下")
+                    } else if (startPoint.y < endPoint.y) {
+                        log("向上")
+                    }
+                }
+            }
+        }, this)
+    }
+
+    init() {
+        let initNumbers = [2, 4]
+        let initBlock: number = Math.round(Math.random() * (this.level - 1));
+        if (initBlock == 0) initBlock++;
+        for (let i = 0; i < initBlock; i++) {
+            let x = Math.round(Math.random() * (this.level - 1));
+            let y = Math.round(Math.random() * (this.level - 1));
+            while (this.blocks[x][y].getComponent(Block).getNumber() != 0) {
+                x = Math.round(Math.random() * this.level);
+                y = Math.round(Math.random() * this.level);
+            }
+
+            let number = initNumbers[Math.round(Math.random() * (initNumbers.length - 1))];
+            this.blocks[x][y].getComponent(Block).setNumber(number)
+        }
     }
 }
 
-
+enum Direction {
+    LEFT, RIGHT
+}
